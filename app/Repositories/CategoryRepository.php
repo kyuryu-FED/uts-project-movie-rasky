dcef8f8d35e03da09410b8a65dead08c10d0c89f
kyuryu-FED <raskytrpl@gmail.com>
2026-04-25 23:55:59 +0700

    refactor: move business logic to service

---
+++ app/Repositories/CategoryRepository.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Category;
+use App\Repositories\CategoryRepositoryInterface;
+
+class CategoryRepository implements CategoryRepositoryInterface  {
+
+    protected $model;
+
+    public function __construct(Category $model){
+        $this->model =$model;
+    }
+
+    public function list(){
+        return $this->model->all();
+    }
+        
+}
---
+++ app/Repositories/MovieRepository.php
@@ -0,0 +1,42 @@
+<?php
+namespace App\Repositories;
+
+use App\Models\Movie;
+use App\Repositories\MovieRepositoryInterface;
+
+class MovieRepository implements MovieRepositoryInterface {
+    protected $model;
+
+    public function __construct(Movie $model) {
+        $this-> model = $model;
+    }
+
+    public function create(array $data){
+        return $this->model->create($data);
+    }
+
+    public function find($id){
+        return $this->model->find($id);
+    }
+
+    public function list()
+    {
+        return $this->model->latest();
+    }
+
+    public function update($id, array $data){
+        $movie = $this->model->find($id);
+
+        if ($movie != null){
+            return $movie->update($data);
+        }
+
+        return null;
+    }
+
+    public function delete($id){
+        $movie =$this->model->find($id);
+
+        return $movie->delete($id);        
+    }
+}
